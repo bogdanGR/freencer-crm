19aa01564a1ddc36359ff6011555ff2ff8f42afa
bogdanGR <fant1k450@gmail.com>
2025-08-26 18:11:36 +0300

    sync roles to pass tests

--- app/Listeners/AssignDefaultRole.php
+++ app/Listeners/AssignDefaultRole.php
@@ -2,12 +2,14 @@
 
 namespace App\Listeners;
 
+use Spatie\Permission\Models\Role;
 use Illuminate\Auth\Events\Registered;
 
 class AssignDefaultRole
 {
     public function handle(Registered $event): void
     {
-        $event->user->assignRole('manager'); // default for new signups
+        Role::firstOrCreate(['name' => 'manager']);
+        $event->user->syncRoles(['manager']);
     }
 }
